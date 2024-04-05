a7df66610ca7fe3cebcc94395a79d1821ae02b87
desmachebet <desmachebet@gmail.com>
2024-04-05 17:50:11 +0300

    Dom edit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let mainElement = document.getElementById("main");
+mainElement.parentNode.removeChild(mainElement);
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+let name = "Des"; 
+newHeader.textContent =name + " is the champion";
